2691bd33084707c9a07f5f7dd52f37dc61a19de8
edwin <edwin.portillo.123@outlook.com>
2017-10-15 20:41:23 -0700

    adding token configuration

--- api/models/User.js
+++ api/models/User.js
@@ -1,26 +1,28 @@
 /**
- * User.js
+ * User
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @module      :: Model
+ * @description :: This is the base user model
+ * @docs        :: http://waterlock.ninja/documentation
  */
 
 module.exports = {
 
-  attributes: {
-
-  	name: {
-  		type: 'string'
-  	},
-  	title: {
-  		type: 'string'
-  	},
-  	type: {
-  		type: 'string',
-  		enum: ['admin', 'user'],
-  		defaultsTo: 'user'
-  	}
-
-  }
+  attributes: require('waterlock').models.user.attributes({
+  
+	  	name: {
+	  		type: 'string'
+	  	},
+	  	title: {
+	  		type: 'string'
+	  	},
+	  	type: {
+	  		type: 'string',
+	  		enum: ['admin', 'user'],
+	  		defaultsTo: 'user'
+	  	}
+  }),
+  
+  beforeCreate: require('waterlock').models.user.beforeCreate,
+  beforeUpdate: require('waterlock').models.user.beforeUpdate
 };
-
