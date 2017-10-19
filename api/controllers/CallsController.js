b9e9cff9fecc49d051af33a135191d5d6e79057b
edwin <edwin.portillo.123@outlook.com>
2017-10-18 19:27:29 -0700

    adding calls rest api

---
+++ api/controllers/CallsController.js
@@ -0,0 +1,11 @@
+/**
+ * CallsController
+ *
+ * @description :: Server-side logic for managing calls
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
